374ef1b19c8dfabf07da80af1080f6a200a8fd84
metshcam <73416650+metshcam@users.noreply.github.com>
2020-11-15 00:48:34 -0500

    Update and rename Get-VMSwitchInterface.ps1 to Create-VirtualSwitch.ps1

--- PowerShell/Create-VirtualSwitch.ps1
+++ PowerShell/Create-VirtualSwitch.ps1
@@ -2,30 +2,43 @@
 ## Function to gather information about HyperV environment
 ####
 
-## HyperV host ComputerName variable (DNS if done remotely)
-##
-$hv_hostname = "hyperv-host"
-
 ## Path where HyperV virtual network environment information is to be provided
+## JSON file on github
 ##
 $hv_switchinfo_json = 'https://raw.githubusercontent.com/metshcam/AutoDeployHyperVLinuxEnvironment/main/JSON/virtualnetwork_private.json'
 $hv_switchinfo = Invoke-WebRequest -Uri $hv_switchinfo_json
+$hv_switchinfo.Content
+
+## Build argument array for the virtual network creation argument values
+##
+$hv_switchargs = $hv_switchinfo.Content | ConvertFrom-Json
+
+## HyperV host (-ComputerName)
+##
+$hv_hostname = "hyperv-host"
 
 ## Validate existing network environment for the host
+## Do not display the Default Switch
 ##
 $hv_switchlist = Get-VMSwitch | Where-Object {$_.Name -ne "Default Switch"}
 
+## Check and display count for existing virtual networks
+## If there aren't any, move on.
+##
 if ($hv_switchlist.Count -eq 0) {
   Write-Host "
   You don't currently have any virtual network
-  environments to worry about.  Creating your 
-  new virtual network without worry.
-"
+  environments.  Creating your new virtual network.
+  "
 }
 
+## If there are about none, display and move on.
+##
 if ($hv_switchlist.Count -gt 0) {
   Write-Host "
   Your environment currently has $hv_switchlist.Count networks.
   We will check and see if there will be a conflict using $hv_switchinfo.
   "
 }
+
+$hv_vmswitch_interfacename
