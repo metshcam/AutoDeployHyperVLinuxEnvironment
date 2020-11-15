b594f2cb8868a0d34970c3c850998b39742d6876
metshcam <73416650+metshcam@users.noreply.github.com>
2020-11-14 23:43:52 -0500

    Update Get-VMSwitchInterface.ps1

--- PowerShell/Get-VMSwitchInterface.ps1
+++ PowerShell/Get-VMSwitchInterface.ps1
@@ -11,11 +11,6 @@ $hv_hostname = "hyperv-host"
 $hv_switchinfo_json = 'https://raw.githubusercontent.com/metshcam/AutoDeployHyperVLinuxEnvironment/main/JSON/virtualnetwork_private.json'
 $hv_switchinfo = Invoke-WebRequest -Uri $hv_switchinfo_json
 
-
-
-$hv_switchinfojson = 
-$hv_switchinfo_path = Join-Path $PSScriptRoot "script-config.json"
-
 $hv_switchlist = Get-VMSwitch | Where-Object {$_.Name -ne "Default Switch"}
 
 if ($hv_switchlist.Count -eq 0) {
