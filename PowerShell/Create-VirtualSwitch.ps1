3aaedaa942e5b0a8c06ea3dc99be1e828657007d
metshcam <73416650+metshcam@users.noreply.github.com>
2020-11-15 00:52:01 -0500

    Update Create-VirtualSwitch.ps1

--- PowerShell/Create-VirtualSwitch.ps1
+++ PowerShell/Create-VirtualSwitch.ps1
@@ -5,7 +5,7 @@
 ## Path where HyperV virtual network environment information is to be provided
 ## JSON file on github
 ##
-$hv_switchinfo_json = 'https://raw.githubusercontent.com/metshcam/AutoDeployHyperVLinuxEnvironment/main/JSON/virtualnetwork_private.json'
+$hv_switchinfo_json = 'https://raw.githubusercontent.com/metshcam/AutoDeployHyperVLinuxEnvironment/main/JSON/hv_ps_switchparameters.json'
 $hv_switchinfo = Invoke-WebRequest -Uri $hv_switchinfo_json
 $hv_switchinfo.Content
 
