2df096fd6a575bea381f607557f01cc08fd6d21d
metshcam <73416650+metshcam@users.noreply.github.com>
2020-11-14 23:44:26 -0500

    Update Get-VMSwitchInterface.ps1

--- PowerShell/Get-VMSwitchInterface.ps1
+++ PowerShell/Get-VMSwitchInterface.ps1
@@ -11,16 +11,21 @@ $hv_hostname = "hyperv-host"
 $hv_switchinfo_json = 'https://raw.githubusercontent.com/metshcam/AutoDeployHyperVLinuxEnvironment/main/JSON/virtualnetwork_private.json'
 $hv_switchinfo = Invoke-WebRequest -Uri $hv_switchinfo_json
 
+## Validate existing network environment for the host
+##
 $hv_switchlist = Get-VMSwitch | Where-Object {$_.Name -ne "Default Switch"}
 
 if ($hv_switchlist.Count -eq 0) {
   Write-Host "
   You don't currently have any virtual network
   environments to worry about.  Creating your 
-  new 
+  new virtual network without worry.
 "
 }
 
 if ($hv_switchlist.Count -gt 0) {
-  Write-Host "Your environment currently has $hv_switchlist.Count networks
+  Write-Host "
+  Your environment currently has $hv_switchlist.Count networks.
+  We will check and see if there will be a conflict using $hv_switchinfo.
+  "
 }
